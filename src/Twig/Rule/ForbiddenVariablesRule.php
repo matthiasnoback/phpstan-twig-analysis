2eb6aae537c53f77b50627b3fc74f97b084f89c3
Matthias Noback <matthiasnoback@gmail.com>
2022-05-10 13:51:42 +0200

    Rule: forbidden vars
    This fixes #3

---
+++ src/Twig/Rule/ForbiddenVariablesRule.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpStanTwigAnalysis\Twig\Rule;
+
+use PhpStanTwigAnalysis\Twig\TwigError;
+use PhpStanTwigAnalysis\Twig\TwigRule;
+use Twig\Node\Expression\NameExpression;
+use Twig\Node\Node;
+
+final class ForbiddenVariablesRule implements TwigRule
+{
+    /**
+     * @param array<string> $forbiddenVariables
+     */
+    public function __construct(private array $forbiddenVariables)
+    {
+    }
+
+    public function getNodeType(): string
+    {
+        return NameExpression::class;
+    }
+
+    /**
+     * @param NameExpression $node
+     */
+    public function processNode(Node $node): array
+    {
+        $variableName = $node->getAttribute('name');
+
+        if (! in_array($variableName, $this->forbiddenVariables, true)) {
+            return [];
+        }
+
+        return [
+            new TwigError(
+                sprintf('Forbidden variable used: %s', $variableName),
+                $node->getSourceContext(),
+                $node->getTemplateLine(),
+            ),
+        ];
+    }
+}
---
+++ tests/Twig/Rule/ForbiddenVariablesRule/ForbiddenVariablesRuleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpStanTwigAnalysis\Twig\Rule\ForbiddenVariablesRule;
+
+use PhpStanTwigAnalysis\Twig\Rule\AbstractTwigRuleTest;
+
+final class ForbiddenVariablesRuleTest extends AbstractTwigRuleTest
+{
+    public function testSkipTemplateUsesNoForbiddenVariables(): void
+    {
+        $this->assertTwigAnalysisErrors('skip-template-uses-no-forbidden-variables.html.twig', []);
+    }
+
+    public function testTemplateUsesForbiddenVariables(): void
+    {
+        $this->assertTwigAnalysisErrors(
+            'template-uses-forbidden-variables.html.twig',
+            [['Forbidden variable used: app', 2], ['Forbidden variable used: app', 5]]
+        );
+    }
+
+    protected static function getExtraConfigFilePathname(): string
+    {
+        return __DIR__ . '/phpstan.neon';
+    }
+}
