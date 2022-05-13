a02987212d000c13d636b0ae115355e882a1a71a
Tomas Votruba <tomas.vot@gmail.com>
2022-05-13 17:39:10 +0200

    [ecs] remove phpunit breaking fixer, as never registered

--- ecs.php
+++ ecs.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PHP_CodeSniffer\Standards\Generic\Sniffs\CodeAnalysis\AssignmentInConditionSniff;
 use PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocAnnotationRemoveFixer;
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
 use Symplify\CodingStandard\Fixer\Naming\StandardizeHereNowDocKeywordFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
@@ -15,8 +14,6 @@ return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([SetList::CONTROL_STRUCTURES, SetList::PSR_12, SetList::COMMON, SetList::SYMPLIFY]);
 
     $ecsConfig->skip([
-        PhpUnitStrictFixer::class,
-        // Because it makes no sense ;) (well, I just need assertEquals())
         // fixture files
         __DIR__ . '/tests/PhpStan/Fixtures',
         __DIR__ . '/tests/Symfony/var',
